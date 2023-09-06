77d84c6b66f8dc6e4b70d75f5bca3380d37f81d2
marcocareddu <saldumaccu@tuta.io>
2023-09-06 16:21:14 +0200

    Fixed database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
 
-        $this->call(UserSeeder::class, Project::class);
+        $this->call([UserSeeder::class, TypeSeeder::class, Project::class]);
     }
 }
